da374d1c58874f9c3fd6ce7f73f9a20cabd42953
Daniel <38920805+VextoN@users.noreply.github.com>
2020-03-10 17:36:20 +0000

    ligeiro upgrade

--- testzhed/MyBoard.cs
+++ testzhed/MyBoard.cs
@@ -5,30 +5,42 @@ namespace HelloWorld
 {
     public class MyBoard
     {
-        List<List<int>> board;
+        private List<List<int>> board = new List<List<int>>{};
         private int width,height;
-
+        private List<int[]> valueTiles;
+        private List<int[]> finishTiles;
         
-        public MyBoard(){
-            
-            width = 5;
-            height = 5;
-            board = new List<List<int>>{
-                new List<int> {0,0,0,3,0},
-                new List<int> {2,0,0,0,0},
-                new List<int> {0,0,0,0,0},
-                new List<int> {0,2,0,0,0},
-                new List<int> {0,0,0,-2,0}  
-            }; 
+        public MyBoard(int width, int height, List<int[]> valueTiles, List<int[]> finishTiles){
+            this.width = width;
+            this.height = height;
+            this.valueTiles = valueTiles;
+            this.finishTiles = finishTiles;
+            makeEmptyBoard();
+            insertValueTiles();
+            insertFinishTiles();
         }
 
-        public MyBoard(int width, int heigth){
+        private void makeEmptyBoard(){
+            for(var i = 0; i < this.width; i++){
+                this.board.Add(new List<int>());
+                for(var j = 0; j < this.height; j++){
+                      this.board[i].Add(0);
+                }
+            }
+        }
+        private void insertValueTiles(){
+            foreach (var tile in valueTiles)
+                this.board[tile[0]][tile[1]] = tile[2];
+        }
 
+        private void insertFinishTiles(){
+            foreach (var tile in finishTiles)
+                this.board[tile[0]][tile[1]] = -2;
         }
 
         public void print(){
-            for(int i = 0; i < width; i++){
-                for(int j=0; j < height; j++){
+            for(int j=0; j < height; j++){
+                for(int i = 0; i < width; i++){
                     Console.Write(board[i][j]);
                     Console.Write(" ");
                 }
@@ -39,9 +51,9 @@ namespace HelloWorld
         public void goUp(int[] coordinates){
             int x = coordinates[0];
             int y = coordinates[1];
-            int tileValue = board[y][x];
+            int tileValue = board[x][y];
             if(tileValue>0){
-                board[y][x] = -1;
+                board[x][y] = -1;
                 untapLoop(tileValue,coordinates,moveUp);
             }
         }
@@ -49,9 +61,9 @@ namespace HelloWorld
         public void goDown(int[] coordinates){
             int x = coordinates[0];
             int y = coordinates[1];
-            int tileValue = board[y][x];
+            int tileValue = board[x][y];
             if(tileValue>0){
-                board[y][x] = -1;
+                board[x][y] = -1;
                 untapLoop(tileValue,coordinates,moveDown);
             }
         }
@@ -59,9 +71,9 @@ namespace HelloWorld
         public void goLeft(int[] coordinates){
             int x = coordinates[0];
             int y = coordinates[1];
-            int tileValue = board[y][x];
+            int tileValue = board[x][y];
             if(tileValue>0){
-                board[y][x] = -1;
+                board[x][y] = -1;
                 untapLoop(tileValue,coordinates,moveLeft);
             }
         }
@@ -69,9 +81,9 @@ namespace HelloWorld
         public void goRight(int[] coordinates){
             int x = coordinates[0];
             int y = coordinates[1];
-            int tileValue = board[y][x];
+            int tileValue = board[x][y];
             if(tileValue>0){
-                board[y][x] = -1;
+                board[x][y] = -1;
                 untapLoop(tileValue,coordinates,moveRight);
             }
         }
@@ -98,13 +110,12 @@ namespace HelloWorld
                 coordinates = moveFunction(coordinates);
                 if(!inbounds(coordinates))
                     break;
-                if(board[coordinates[1]][coordinates[0]]==0){
-                    Console.WriteLine(board[coordinates[1]][coordinates[0]]);
-                    board[coordinates[1]][coordinates[0]]=-1;
+                if(board[coordinates[0]][coordinates[1]]==0){
+                    board[coordinates[0]][coordinates[1]]=-1;
                     tileValue--;
                 }
-                if(board[coordinates[1]][coordinates[0]]==-2){
-                    board[coordinates[1]][coordinates[0]]=-3;
+                if(board[coordinates[0]][coordinates[1]]==-2){
+                    board[coordinates[0]][coordinates[1]]=-3;
                     Console.WriteLine("Finished");
                 }
             }
--- testzhed/Program.cs
+++ testzhed/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            var b1 = new MyBoard();
+        {   
+
+            List<int[]> valueTiles =  new List<int[]> {
+                new int[] { 2, 2, 1},
+                new int[] { 1, 3, 2},
+                new int[] { 0, 0, 3}
+            };
+            List<int[]> finishTiles =  new List<int[]> {
+                new int[] {4, 0}
+            };
+
+            var b1 = new MyBoard(5,4,valueTiles,finishTiles);
             b1.print();
+            
+            b1.goLeft(new int[]{2,2});
+            Console.WriteLine();
+            b1.print();
+
             b1.goUp(new int[]{1,3});
             Console.WriteLine();
             b1.print();
-            b1.goRight(new int[]{0,1});
-            Console.WriteLine();
-            b1.print();
-            b1.goDown(new int[]{3,0});
+
+            b1.goRight(new int[]{0,0});
             Console.WriteLine();
             b1.print();
         }
